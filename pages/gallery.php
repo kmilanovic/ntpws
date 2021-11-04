08339c60a1c555c63bb1284e1b1ea67bfedd17bd
DESKTOP-H4C386B\Kristijan <krisitjan.milanovic1@gmail.com>
2021-11-04 17:01:46 +0100

    gallery edited

--- pages/gallery.php
+++ pages/gallery.php
@@ -1,95 +1,103 @@
+<h1>Gallery</h1>
 
-      <h1>Gallery</h1>
+<div class="image">
+<figure id="galleryFigure">
+  <a target="_blank" href="assets/img/news1.jpg">
+    <img/img src="assets/img/news1.jpg" alt="Chemberson's goal celebration">
+  </a>
+  <figcaption>Chemberson's goal celebration</figcaption>
+</figure>
+</div>
 
-      <figure id="galleryFigure">
-        <a target="_blank" href="assets/img/news1.jpg">
-          <img/img src="assets/img/news1.jpg" alt="Chemberson's goal celebration"
-          width="600" height="400">
-        </a>
-        <figcaption>Royal Arsenal's squad of the 1888–89 season</figcaption>
-      </figure>
+<div class="image">
+<figure id="galleryFigure">
+  <a target="_blank" href="assets/img/news2_1.jpg">
+    <img/img src="assets/img/news2_1.jpg" alt="Charlie Patino">
+  </a>
+  <figcaption>Charlie Patino</figcaption>
+</figure>
+</div>
 
-      <figure id="galleryFigure">
-         <a target="_blank" href="assets/img/news2_1.jpg">
-          <img/img src="assets/img/news2_1.jpg" alt="Charlie Patino" width="600"
-          height="400">
-        </a>
-        <figcaption>Charlie Patino</figcaption>
-      </figure>
+<div class="image">
+<figure id="galleryFigure">
+  <a target="_blank" href="assets/img/news2_2.jpg">
+    <img/img src="assets/img/news2_2.jpg" alt="U-18">
+  </a>
+  <figcaption>U-18</figcaption>
+</figure>
+</div>
 
-      <figure id="galleryFigure">
-        <a target="_blank" href="assets/img/news2_2.jpg">
-          <img/img src="assets/img/news2_2.jpg" alt="U-18" width="600" height="400">
-        </a>
-        <figcaption>U-18</figcaption>
-      </figure>
+<div class="image">
+<figure id="galleryFigure">
+  <a target="_blank" href="assets/img/news2_3.jpg">
+    <img/img src="assets/img/news2_3.jpg" alt="U-23">
+  </a>
+  <figcaption>U-23</figcaption>
+</figure>
+</div>
 
-     <figure id="galleryFigure">
-      <a target="_blank" href="assets/img/news2_3.jpg">
-        <img/img src="assets/img/news2_3.jpg" alt="U-23" width="600" height="400">
-      </a>
-      <figcaption>U-23</figcaption>
-    </figure>
+<div class="image">
+<figure id="galleryFigure">
+  <a target="_blank" href="assets/img/news2_4.jpg">
+    <img/img src="assets/img/news2_4.jpg" alt="Road to pitch">
+  </a>
+  <figcaption>Road to pitch</figcaption>
+</figure>
+</div>
 
-    <figure id="galleryFigure">
-      <a target="_blank" href="assets/img/news2_4.jpg">
-        <img/img src="assets/img/news2_4.jpg" alt="Road to pitch" width="600"
-        height="400">
-      </a>
-      <figcaption>Road to pitch</figcaption>
-    </figure>
+<div class="image">
+<figure id="galleryFigure">
+  <a target="_blank" href="assets/img/news3_2.jpg">
+    <img/img src="assets/img/news3_2.jpg" alt="Eddie Nketiah">
+  </a>
+  <figcaption>Eddie Nketiah</figcaption>
+</figure>
+</div>
 
-    <figure id="galleryFigure">
-      <a target="_blank" href="assets/img/news3_2.jpg">
-        <img/img src="assets/img/news3_2.jpg" alt="Eddie Nketiah" width="600"
-        height="400">
-      </a>
-      <figcaption>Eddie Nketiah</figcaption>
-    </figure>
 
-    <figure id="galleryFigure">
-      <a target="_blank" href="assets/img/news3_3.jpg">
-        <img/img src="assets/img/news3_3.jpg" alt="Ben White" width="600"
-        height="400">
-      </a>
-      <figcaption>Ben White</figcaption>
-    </figure>
+<div class="image">
+  <figure id="galleryFigure">
+  <a target="_blank" href="assets/img/news3_3.jpg">
+    <img/img src="assets/img/news3_3.jpg" alt="Ben White">
+  </a>
+  <figcaption>Ben White</figcaption>
+</figure>
+</div>
 
-    <figure id="galleryFigure">
-      <a target="_blank" href="assets/img/news4_1.jpg">
-        <img/img src="assets/img/news4_1.jpg" alt="Jimmy and ESR" width="600"
-        height="400">
-      </a>
-      <figcaption>Jimmy and ESR</figcaption>
-    </figure>
+<div class="image">
+<figure id="galleryFigure">
+  <a target="_blank" href="assets/img/news4_1.jpg">
+    <img/img src="assets/img/news4_1.jpg" alt="Jimmy and ESR">
+  </a>
+  <figcaption>Jimmy and ESR</figcaption>
+</figure>
+</div>
 
-    <figure id="galleryFigure">
-      <a target="_blank" href="assets/img/news4_2.jpg">
-        <img/img src="assets/img/news4_2.jpg" alt="Forest" width="600" height="400">
-      </a>
-      <figcaption>Jimmy, Jimmy's father Chris and ESR</figcaption>
-    </figure>
+<div class="image">
+<figure id="galleryFigure">
+  <a target="_blank" href="assets/img/news4_2.jpg">
+    <img/img src="assets/img/news4_2.jpg" alt="Forest">
+  </a>
+  <figcaption>Jimmy, Jimmy's father Chris and ESR</figcaption>
+</figure>
+</div>
 
-    <figure id="galleryFigure">
-      <a target="_blank" href="assets/img/news5_1.jpg">
-        <img/img src="assets/img/news5_1.jpg" alt="Goal celebration" width="600"
-        height="400">
-      </a>
-      <figcaption>Goal celebration</figcaption>
-    </figure>
+<div class="image">
+  <figure id="galleryFigure">
+  <a target="_blank" href="assets/img/news5_1.jpg">
+    <img/img src="assets/img/news5_1.jpg" alt="Goal celebration">
+  </a>
+  <figcaption>Goal celebration</figcaption>
+</figure>
+</div>
 
-    <figure id="galleryFigure">
-      <a target="_blank" href="assets/img/news5_2.jpg">
-        <img/img src="assets/img/news5_2.jpg" alt="Forest" width="600" height="400">
-      </a>
-      <figcaption>Vivianne Miedema</figcaption>
-    </figure>
-    
-    </main>
-    <footer>
-      <p>
-        Copyright © 2021 Kristijan Milanović
-        <a href="https://github.com/kmilanovic" target="_blank"
-          ><i class="fab fa-github"></i
-        ></a>
-      </p>
+<div class="image">
+  <figure id="galleryFigure">
+  <a target="_blank" href="assets/img/news5_2.jpg">
+    <img/img src="assets/img/news5_2" alt="Viviene Miedema">
+</a>
+    <figcaption>
+      Viviene Miedema
+    </figcaption>
+  </figure>
+</div>
