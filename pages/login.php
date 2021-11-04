9d5c2843b96b8257ccdf3a48665a1b2d4991748d
DESKTOP-H4C386B\Kristijan <krisitjan.milanovic1@gmail.com>
2021-11-04 17:00:04 +0100

    added register and login pages

---
+++ pages/login.php
@@ -0,0 +1,14 @@
+<div class="form">
+        <form action="">
+          <label for="username">Username</label>
+          <input type="text" id="username" name="username" placeholder="Username" />
+
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Password" />
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div> 
+
+
+
---
+++ pages/register.php
@@ -0,0 +1,38 @@
+
+<div class="form">
+        <form action="">
+          <label for="fname">First Name</label>
+          <input type="text" id="fname" name="firstname" placeholder="Your first name" />
+
+          <label for="lname">Last Name</label>
+          <input type="text" id="lname" name="lastname" placeholder="Your last name" />
+
+          <label for="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="Your email" />
+
+
+          <label for="country">Country</label>
+          <div class="select">
+            <select class="countries">
+            <option value="">Select country</option>
+            <option value="Croatia">Croatia</option>
+            <option value="Bosnia and Herzegovina">Bosnia and Herzegovina</option>
+            <option value="Serbia">Serbia</option>
+            </select>
+          </div>
+
+          <label for="city">City</label>
+          <input type="text" id="city" name="city" placeholder="City" />
+
+          <label for="street">Street</label>
+          <input type="text" id="street" name="street" placeholder="Street" />
+
+          <label for="dob">Street</label>
+          <input type="date" id="dob" name="dob" placeholder="DOB" />
+
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Password" />
+
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
