ee0d7056609c64ff51aa42c08fcd6c1e2065ac11
DESKTOP-H4C386B\Kristijan <krisitjan.milanovic1@gmail.com>
2021-11-08 20:15:33 +0100

    refactor

--- index.php
+++ index.php
@@ -1,39 +1,54 @@
+<?php
+
+include('config/connection.php');
+
+session_start();
+
+if(isset($_GET['menu'])) { $menu   = (int)$_GET['menu']; }
+	if(isset($_GET['action'])) { $action   = (int)$_GET['action']; }
+
+if(!isset($_POST['_action_']))  { $_POST['_action_'] = FALSE;  }
+
+
+print '
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title>Napredne tehnike projektiranja web servisa</title>
-    <meta http-equiv="content-type" content="text.php; charset=UTF-8" />
-    <meta name="description" content="Opis stranice" />
-    <meta name="keywords" content="ključna riječ1, ključna riječ2" />
-    <meta name="author" content="Kristijan Milanović" />
-    <meta name="viewport" content="width=device-width, inital-scale=1.0" />
-    <link rel="stylesheet" href="assets/css/style.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+<title>Napredne tehnike projektiranja web servisa</title>
+<meta http-equiv="content-type" content="text.php; charset=UTF-8" />
+<meta name="description" content="Opis stranice" />
+<meta name="keywords" content="ključna riječ1, ključna riječ2" />
+<meta name="author" content="Kristijan Milanović" />
+<meta name="viewport" content="width=device-width, inital-scale=1" />
+<link rel="stylesheet" href="assets/css/style.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 </head>
 
-<body>
-    <?php
-    include('includes/header.php');
-    ?>
-    <main>
-        <?php
-        if (!isset($_GET['menu']) || $_GET['menu'] == 'home') {
-            include("pages/home.php");
-        } else if ($_GET['menu'] == 'news') {
-            include("pages/news.php");
-        } else if ($_GET['menu'] == 'contact') {
-            include("pages/contact.php");
-        } else if ($_GET['menu'] == 'about') {
-            include("pages/about.php");
-        } else if ($_GET['menu'] == 'gallery') {
-            include("pages/gallery.php");
-        }
-        ?>
-    </main>
-    <?php
-    include('includes/footer.php');
-    ?>
+<body> ';
+include('includes/header.php');
+print '    
+<main>';
+if (!isset($_GET['menu']) || $_GET['menu'] == 'home') {
+    include("pages/home.php");
+} else if ($_GET['menu'] == 'news') {
+    include("pages/news.php");
+} else if ($_GET['menu'] == 'contact') {
+    include("pages/contact.php");
+} else if ($_GET['menu'] == 'about') {
+    include("pages/about.php");
+} else if ($_GET['menu'] == 'gallery') {
+    include("pages/gallery.php");
+} else if ($_GET['menu'] == 'register') {
+    include("pages/register.php");
+} else if ($_GET['menu'] == 'login') {
+    include("pages/login.php");
+} else if ($_GET['menu'] == 'admin') {
+    include("pages/admin.php");
+}
+print '        
+</main>';
+include('includes/footer.php');
+print '    
 </body>
-
-<.php>
+</html>';
