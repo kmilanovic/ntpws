9a95f383c4b4f0b0b66eed37b825a87ae2b71392
Kristijan <kristijan.milanovic1@gmail.com>
2021-12-02 03:22:33 +0100

    Refactor login page

--- pages/login.php
+++ pages/login.php
@@ -1,11 +1,12 @@
 <?php 
 	print '
 	<h1>Login</h1>';
+
 	if ($_POST['_action_'] == false) {
 		print '
         <div class="form">
             <form action="" name="myForm" id="myForm" method="POST">
-                <input type="hidden" id="_action_" name="_action_" value="TRUE">
+			<input type="hidden" id="_action_" name="_action_" value="TRUE">
                 <label for="username">Username:</label>
                 <input type="text" id="username" name="username" required>
                                         
@@ -20,17 +21,18 @@
 		
 		$query  = "SELECT * FROM users";
 		$query .= " WHERE username='" .  $_POST['username'] . "'";
-		$result = mysqli_query($conn, $query);
-		$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+		$result = @mysqli_query($conn, $query);
+		$row = @mysqli_fetch_array($result, MYSQLI_ASSOC);
 		
 		if (password_verify($_POST['password'], $row['password'])) {
 			$_SESSION['user']['valid'] = 'true';
 			$_SESSION['user']['id'] = $row['id'];
 			$_SESSION['user']['firstname'] = $row['firstname'];
 			$_SESSION['user']['lastname'] = $row['lastname'];
+			$_SESSION['user']['role'] = $row['role'];
 			$_SESSION['message'] = '<p>Dobrodošli, ' . $_SESSION['user']['firstname'] . ' ' . $_SESSION['user']['lastname'] . '</p>';
 			header("Location: index.php?menu=home");
-		}
+	   }
 		
 		# Bad username or password
 		else {
