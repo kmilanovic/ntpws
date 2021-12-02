4dfef6254818d7e560c7a721704d1b0edf8dec18
Kristijan <kristijan.milanovic1@gmail.com>
2021-12-02 03:21:20 +0100

    Added admin page to menu

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
+    define('__APP__', TRUE);
 session_start();
-
-include('config/connection.php');
-
-if(!isset($_POST['_action_']))  { $_POST['_action_'] = FALSE;  }
+    include('config/connection.php');
+    if(isset($_GET['menu'])) { $menu = (int)$_GET['menu']; }
+if(isset($_GET['action'])) { $action   = (int)$_GET['action']; }
+    if(!isset($_POST['_action_']))  { $_POST['_action_'] = FALSE;  }
 
 print '
 <!DOCTYPE html>
@@ -30,24 +31,20 @@ if (isset($_SESSION['message'])) {
     unset($_SESSION['message']);
 }
 
-if (!isset($_GET['menu']) || $_GET['menu'] == 'home') {
-    include("pages/home.php");
-} else if ($_GET['menu'] == 'news') {
-    include("pages/news.php");
-} else if ($_GET['menu'] == 'contact') {
-    include("pages/contact.php");
-} else if ($_GET['menu'] == 'about') {
-    include("pages/about.php");
-} else if ($_GET['menu'] == 'gallery') {
-    include("pages/gallery.php");
-} else if ($_GET['menu'] == 'register') {
-    include("pages/register.php");
-} else if ($_GET['menu'] == 'login') {
-    include("pages/login.php");
-} else if ($_GET['menu'] == 'logout') {
-    include("pages/logout.php");
-} else if($_GET['menu'] == 'table'){
-    include("pages/table.php");
+if (isset($_GET['menu'])) {
+    if ($_GET['menu'] == 'home') { include("pages/home.php"); }
+	  else if ($_GET['menu'] == 'news') { include("pages/news.php"); }
+	  else if ($_GET['menu'] == 'contact') { include("pages/contact.php"); }
+	  else if ($_GET['menu'] == 'about') { include("pages/about.php"); }
+    else if ($_GET['menu'] == 'gallery') { include("pages/gallery.php"); }
+    else if ($_GET['menu'] == 'register') { include("pages/register.php"); }
+    else if ($_GET['menu'] == 'login') { include("pages/login.php"); }
+    else if ($_GET['menu'] == 'logout') { include("pages/logout.php"); }
+    else if ($_GET['menu'] == 'admin') { include("pages/admin.php"); }
+    else if ($_GET['menu'] == 'table-api') { include("pages/table.php"); }
+   }
+if (isset($_GET['article'])) {
+    include("pages/article.php");
 }
 print '        
 </main>';
