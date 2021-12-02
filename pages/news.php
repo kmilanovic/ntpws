3f33e230fb8b2e9c2b3d4006649a943a75bd8bcf
Kristijan <kristijan.milanovic1@gmail.com>
2021-12-02 03:22:02 +0100

    Added news page

--- pages/news.php
+++ pages/news.php
@@ -1,109 +1,40 @@
 <?php
 print '
 <h1>News</h1>
+';
 
+$query  = "SELECT * FROM news WHERE is_approved='1'";
+$query .= " ORDER BY date DESC";
+$result = @mysqli_query($conn, $query);
+
+while($row = @mysqli_fetch_array($result)) {
+    $queryImage = "SELECT * FROM pictures WHERE newsId='".$row['id']."' LIMIT 1";
+    $resultImage = @mysqli_query($conn, $queryImage);
+    $rowImage = @mysqli_fetch_array($resultImage);
+    
+print '
 <div class="card__wrap--outer">
 <div class="card__wrap--inner">
     <div class="card">
-        <a href="pages/article1.html"><img src="assets/img/news1.jpg"></a>
+        <a href="index.php?article='. $row['id'] .'"><img src="assets/'.$rowImage['img'].'"></a>
         <div class="card__item">
-            <h2>Match report: Arsenal 2-0 Leeds</h2>
+            <h2>'. $row['title'] . '</h2>
         </div>
         <div class="card__date">
-            <small>26th October 2021</small>
+            <small>'. $row['date'] . '</small>
         </div>
         <div class="card__item flexible">
-            <small> We booked our place in the quarter-finals of the Carabao Cup after
-                second half goals from Calum Chambers and Eddie Nketiah saw off the
-                challenge of Leeds.</small>
+            <small class="description" ><span> '. $row['description'] . '</span></small>
         </div>
         <div class="card__footer">
-            <a class="pull" href="pages/article1.html"><small>Read more</small></a>
+            <a class="pull" href="index.php?article='. $row['id'] .'"><small>Read more</small></a>
         </div>
     </div>
 </div>
+</div>';}
 
-<div class="card__wrap--inner">
-    <div class="card">
-    <a href="pages/article2.html"><img src="assets/img/news2_1.jpg"></a>
-        <div class="card__item">
-            <h2>Behind the scenes at the academy photocall</h2>
-        </div>
-        <div class="card__date">
-            <small>26th October 2021</small>
-        </div>
-        <div class="card__item flexible">
-            <small>After Under-18s slipped to their first league defeat of the season on
-                Saturday they posed for their official team photos for the 2021/22
-                season.</small>
-        </div>
-        <div class="card__footer">
-            <a class="pull" href="pages/article2.html"><small>Read more</small></a>
-        </div>
-    </div>
-</div>
-
-<div class="card__wrap--inner">
-    <div class="card">
-    <a href="pages/article3.html"><img src="assets/img/news3_1.jpg"></a>
-        <div class="card__item">
-            <h2>Arteta on Chambers, Nketiah, White\'s fitness</h2>
-        </div>
-        <div class="card__date">
-            <small>26th October 2021</small>
-        </div>
-        <div class="card__item flexible">
-            <small>Mikel Arteta was delighted to see Calum Chambers and Eddie Nketiah
-                settle our tie with Leeds on Tuesday night.</small>
-        </div>
-        <div class="card__footer">
-            <a class="pull" href="pages/article3.html"><small>Read more</small></a>
-        </div>
-    </div>
-</div>
-
-<div class="card__wrap--inner">
-    <div class="card">
-    <a href="pages/article4.html"><img src="assets/img/news4_1.jpg"></a>
-        <div class="card__item">
-            <h2>Arsenal Supporting Supporters: JNF</h2>
-        </div>
-        <div class="card__date">
-            <small>26th October 2021</small>
-        </div>
-        <div class="card__item flexible">
-            <small>Emile Smith Rowe has joined Rob Holding and Pierre Emerick-Aubameyang in
-                supporting local businesses, this time with local haircutters, JNF, as
-                part of the Arsenal Supporting Supporters series.</small>
-        </div>
-        <div class="card__footer">
-            <a class="pull" href="pages/article4.html"><small>Read more</small></a>
-        </div>
-    </div>
-</div>
-
-<div class="card__wrap--inner">
-    <div class="card">
-    <a href="pages/article5.html"><img src="assets/img/news5_1.jpg"></a>
-        <div class="card__item">
-            <h2>Heath - It\'s easy with \'cerebral\' Miedema</h2>
-        </div>
-        <div class="card__date">
-            <small>26th October 2021</small>
-        </div>
-        <div class="card__item flexible">
-            <small>Our 4-0 victory over Hoffenheim proved to be a special night for Tobin
-                Heath as she scored her first goal in Arsenal colours.</small>
-        </div>
-        <div class="card__footer">
-            <a class="pull" href="pages/article5.html"><small>Read more</small></a>
-        </div>
-    </div>
-</div>
-</div>
-
-<p>Social media:<br>
-    <a href="https://www.facebook.com/profile.php?id=100043504936152" target="_blank"><i class="fab fa-facebook-f"></i></a>
-    <a href="https://www.instagram.com/milanovickristijan/" target="_blank"><i class="fab fa-instagram"></i></a>
-</p>';
+print '<p>Social media:<br>
+<a href="https://www.facebook.com/profile.php?id=100043504936152" target="_blank"><i class="fab fa-facebook-f"></i></a>
+<a href="https://www.instagram.com/milanovickristijan/" target="_blank"><i class="fab fa-instagram"></i></a>
+</p>'
 ?>
